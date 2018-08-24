c1099928c03412da1f7652d5d2eb78b52fe820ea
John Michals <jmichals@Dorado.Local>
2018-08-24 13:18:09 -0400

    modified the file

--- pybanktest2.py
+++ pybanktest2.py
@@ -18,19 +18,27 @@ with open(pybank_csv, newline="") as csvfile:
         Total_PNL1 = int(Total_PNL1)
         Total_PNL.append(Total_PNL1)
         Total_PNL2 = sum(Total_PNL)
+        #Different approach, tried to declare a variable, PNL_diff, as subtraction of two elements in the
+        # list, then appended those differences
+        #PNL_diff = ([Total_PNL[0] - Total_PNL[1]])
+        #AVGP.append(PNL_diff)
         #avg = statistics.mean([Total_PNL1])
-        diff = Total_PNL1 + 1 
-        AVGP1 = diff - Total_PNL1
-        AVGP.append(AVGP1)
-        AVGP2 = statistics.mean(AVGP)
+        #diff = Total_PNL1 + 1 
+        #AVGP1 = diff - Total_PNL1
+        #AVGP1 = Total_PNL[1] + Total_PNL[2]
+        #######Also tried to reference the PNL list and add indexs
+        #print(AVGP1)
+        #AVGP.append(AVGP1)
+        #AVGP2 = statistics.mean(AVGP)
         ########I want to take the differences, store in a variable, then run a mean on them
         Total_Months = Total_Months + 1
-       
+        
+#print(AVGP)
 print("Financial Analysis")
 print("-----------------------------------------------------")
 print("Total Months: ", Total_Months)
 print("Total: " + "$" + str((Total_PNL2)))
-print("Average Change: " + "$" + str(AVGP2))
+#print("Average Change: " + "$" + str(AVGP))
 print("Greatest Increase in Profits: " + "$" + str(max(Total_PNL)))
 print("Greatest Decrease in Profits: " + "$" + str(min(Total_PNL)))
         
